c04a1052867bde3e1ca16d1c1e7d1a04f2bb128d
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2022-01-03 09:02:46 +0530

    Initial commit

--- app/src/main/java/com/example/base/helper/demo/MainActivity.kt
+++ app/src/main/java/com/example/base/helper/demo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.base.helper.demo
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.example.app.base.helper.BaseBindingActivity
+import com.example.base.helper.demo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
+
+    override fun getActivityContext(): FragmentActivity {
+        return this@MainActivity
+    }
+
+    override fun setBinding(): ActivityMainBinding {
+        return ActivityMainBinding.inflate(layoutInflater)
     }
 }
