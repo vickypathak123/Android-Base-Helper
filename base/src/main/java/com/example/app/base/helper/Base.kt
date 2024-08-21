d57cdb8a19c29f3abaab49be0a02b42fcb056eaa
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2024-08-21 16:55:36 +0530

    add 2nd module for testing

---
+++ base/src/main/java/com/example/app/base/helper/Base.kt
@@ -0,0 +1,3 @@
+package com.example.app.base.helper
+
+const val baseModuleName: String = "Base Module Success"
---
+++ testing/src/main/java/com/example/app/base/helper/Testing.kt
@@ -0,0 +1,3 @@
+package com.example.app.base.helper
+
+const val testingModuleName: String = "Testing Module Success"
